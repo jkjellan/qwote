f67b4b8a7f492ac6f31fa5b22c1acda97d44f2a6
Jim Kjelland <Jim Kjelland>
2016-12-06 20:42:14 -0800

    added first html with bootstrap css

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -4,7 +4,17 @@ export default class Layout extends React.Component{
   render(){
     return(
       <div>
-        <h1> Hello </h1>
+        <div class="container-fluid text-center">
+    			<div class = "row">
+      			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      			</div>
+      			<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+      				<input class = "form-control"></input>
+      			</div>
+      			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+      			</div>
+    			</div>
+    		</div>
       </div>
     );
   }
